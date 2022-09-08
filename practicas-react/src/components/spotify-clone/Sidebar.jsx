e8e755c615a2a1c019262584aba6faba3814c9df
Sebastian Rios <riossebastianr@gmail.com>
2022-09-08 12:35:21 -0300

    component Sidebar styles updated

--- practicas-react/src/components/spotify-clone/Sidebar.jsx
+++ practicas-react/src/components/spotify-clone/Sidebar.jsx
@@ -3,12 +3,28 @@ import { VStack, Flex } from "@chakra-ui/react";
 
 function Sidebar() {
   return (
-    <VStack bg="black" h="100vh" w="15%" p="8">
-      <Flex h="40vh" bg="gray.200" w="100%">
-        <h2>menu 1</h2>
+    <VStack bg="black" h="100vh" w="15%" p="2">
+      <Flex color="red" bg="white" h="12vh" w="100%">
+        <h2>prox. a colocar simbolo + link</h2>
       </Flex>
-      <Flex h="100vh" bg="gray.500" w="100%">
+      <Flex bg="black" h="35vh" w="100%">
         <h2>menu 2</h2>
+      </Flex>
+      <Flex
+        h="35vh"
+        bg="black"
+        w="100%"
+        borderTopWidth={1}
+        borderTopColor="#444"
+        pt="4"
+        direction="column"
+      >
+        <h2>Create list</h2>
+        <h2>Songs you liked</h2>
+      </Flex>
+      <Flex h="20vh" bg="gray.500" w="100%">
+        <h2>Cookies</h2>
+        <h2>Privacity</h2>
       </Flex>
     </VStack>
   );
