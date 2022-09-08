055cb2ba5846a11602998f63f8eeafb3f7b23825
Sebastian Rios <riossebastianr@gmail.com>
2022-09-08 12:34:13 -0300

    component spotify-clone updated

--- practicas-react/src/components/spotify-clone/Content.jsx
+++ practicas-react/src/components/spotify-clone/Content.jsx
@@ -3,11 +3,20 @@ import { VStack, Flex } from "@chakra-ui/react";
 
 function Content() {
   return (
-    <VStack bg="black" h="100vh" w="85%">
-      <Flex bg="gray.200" w="100%" h="20vh">
+    <VStack h="100vh" w="85%">
+      <Flex
+        bg="#333"
+        w="100%"
+        h="65px"
+        pos="fixed"
+        top={0}
+        left="15%"
+        align="center"
+        px={8}
+      >
         <h2>HEADER FIXED</h2>
       </Flex>
-      <Flex h="190vh" bg="black" w="100%">
+      <Flex h="190vh" bg="#111" w="100%" px={8}>
         <h2>CONTENT</h2>
       </Flex>
     </VStack>
