159d8b9f286538f2551743ebf62069dcd3f5c62d
Sebastian Rios <riossebastianr@gmail.com>
2022-09-12 08:04:35 -0300

    add component "search menu" inside sidebar

---
+++ practicas-react/src/components/sidebar_menu/search_menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box } from "@chakra-ui/input";
+
+function SearchMenu() {
+  return;
+
+  <Link to={linkTo}>
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Icon />
+      <Text>"text"</Text>
+    </Box>
+  </Link>;
+}
+
+export default SearchMenu;
