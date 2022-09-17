90ab92d92448f58b52577ad837cd2d2f503ae0c6
Sebastian Rios <riossebastianr@gmail.com>
2022-09-16 21:12:42 -0300

    added comments for future routes

--- practicas-react/src/App.jsx
+++ practicas-react/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SpotifyClone from "./pages/SpotifyClone";
 import NetflixClone from "./pages/NetflixClone";
-import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import MenuCollection from "./components/spotify-clone/sidebar-menu/MenuCollection";
+// import Cookies from "./spotify-clone/cookies";
 
 function App() {
   return (
@@ -16,9 +18,18 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/spotify-clone" element={<SpotifyClone />} />
+          <Route path="/spotify-clone" element={<SpotifyClone />}>
+            {/* <Route
+              path="/components/spotify-clone/sidebar-menu/MenuCollection"
+              element={<MenuCollection />}
+            /> */}
+            {/* <Route path="/collection/tracks" element={<Tracks />} /> */}
+          </Route>
+          {/* <Route path="/spotify-clone/cookies" element={<Cookies />} /> */}
+          {/* <Route path="/spotify-clone/privacy-policy" element={<Privacy />} /> */}
           <Route path="/netflix-clone" element={<NetflixClone />} />
           <Route path="/Register" element={<Register />} />
+          <Route />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
