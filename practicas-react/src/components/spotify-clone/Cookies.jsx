cb178abbad395d58be70ddc88fa9c3500104b4a6
Sebastian Rios <riossebastianr@gmail.com>
2022-09-16 21:16:13 -0300

    add cookies page

---
+++ practicas-react/src/components/spotify-clone/Cookies.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Flex, Text, Icon } from "@chakra-ui/react";
+
+function Cookies() {
+  return (
+    // <Link w="100%" to={linkTo}>
+    //   <Flex bg="black" w="100%" p="2" ml="3">
+    //     <Icon as={icon} w={7} h={7} mr="3" />
+    //     <Text fontSize="md " textAlign="left" as="samp" mt="0.5">
+    //       {text}
+    //     </Text>
+    //   </Flex>
+    //   <Outlet />
+    // </Link>
+    <h1>hola</h1>
+  );
+}
+
+export default Cookies;
