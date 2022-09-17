d48c62db7287f87e4492f98734f701e2b2f361e7
Sebastian Rios <riossebastianr@gmail.com>
2022-09-16 21:13:46 -0300

    update content

--- practicas-react/src/components/spotify-clone/Content.jsx
+++ practicas-react/src/components/spotify-clone/Content.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { VStack, Flex } from "@chakra-ui/react";
+import MenuCollection from "./sidebar-menu/MenuCollection";
+import { Outlet } from "react-router-dom";
 
 function Content() {
   return (
-    <VStack h="100vh" w="85%">
+    <VStack h="85vh" w="80%" p="0">
       <Flex
         bg="#333"
         w="100%"
-        h="65px"
+        h="10vh"
         pos="fixed"
         top={0}
-        left="15%"
+        left="20%"
         align="center"
         px={8}
       >
         <h2>HEADER FIXED</h2>
       </Flex>
-      <Flex h="190vh" bg="#111" w="100%" px={8}>
-        <h2>CONTENT</h2>
+      <Flex h="100vh" bg="#111" w="100%" px={8}>
+        <h1> Content</h1>
       </Flex>
     </VStack>
   );
