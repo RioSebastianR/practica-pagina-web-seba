5f3d548336b453f6b18a197723a5a502202ec4c9
Sebastian Rios <riossebastianr@gmail.com>
2022-09-16 21:10:46 -0300

    update sidebar component

--- practicas-react/src/components/spotify-clone/Sidebar.jsx
+++ practicas-react/src/components/spotify-clone/Sidebar.jsx
@@ -1,28 +1,35 @@
 import React from "react";
-import { VStack, Flex, Text, Link, Button } from "@chakra-ui/react";
+import { VStack, Flex } from "@chakra-ui/react";
+import { HiHome, HiSearch, HiLibrary } from "react-icons/hi";
+import { RiAddBoxFill } from "react-icons/ri";
+import { FaHeartbeat } from "react-icons/fa";
+import { BsSpotify } from "react-icons/bs";
+import MenuItem from "./sidebar-menu/MenuItem";
+import MenuCollection from "./sidebar-menu/MenuCollection";
+import { Outlet } from "react-router-dom";
+// import Cookies from "/spotify-clone/cookies";
+
+// import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <VStack bg="black" h="100vh" w="15%" p="2">
-      <Flex color="red" bg="white" h="12vh" w="100%">
-        <Text>
-          "Icono + "
-          <Link color="blue.500" href="/spotify-clone">
-            {" "}
-            Link
-          </Link>
-        </Text>
+    <VStack bg="black" h="100vh" w="20%" p="2" spacing={10}>
+      <Flex w="100%" h="2vh" p="0" m="0" direction="column">
+        <MenuItem icon={BsSpotify} text="Spotify" linkTo="/spotify-clone" />
       </Flex>
-      <Flex direction="column">
-        <Button color="white" bgColor="blackAlpha.200">
-          Home
-        </Button>
-        <Button color="white" bgColor="blackAlpha.200">
-          Search
-        </Button>
-        <Button color="white" bgColor="blackAlpha.200">
-          My Library
-        </Button>
+      <Flex h="35vh" w="100%" direction="column">
+        <MenuItem icon={HiHome} text="Home" linkTo="/spotify-clone" />
+        <MenuItem
+          icon={HiSearch}
+          text="Search"
+          linkTo="/spotify-clone/search"
+        />
+        <MenuCollection
+          icon={HiLibrary}
+          text="Library"
+          linkTo="/spotify-clone/collection"
+        />
+        <Outlet />
       </Flex>
       <Flex
         h="35vh"
@@ -33,12 +40,22 @@ function Sidebar() {
         pt="4"
         direction="column"
       >
-        <h2>Create list</h2>
-        <h2>Songs you liked</h2>
+        <MenuItem
+          icon={RiAddBoxFill}
+          text="Create list"
+          linkTo="/spotify-clone"
+        />
+        <MenuCollection
+          icon={FaHeartbeat}
+          text="Songs you liked"
+          linkTo="/spotify-clone/collection/tracks"
+        />
       </Flex>
-      <Flex h="20vh" bg="gray.500" w="100%">
+      <Flex h="20vh" bg="gray.500" w="100%" direction="column">
         <h2>Cookies</h2>
         <h2>Privacity</h2>
+        {/* <Cookies text="Cookies" linkTo="/spotify-clone/cookies" /> */}
+        {/* <Link text="Spotify" linkTo="/spotify-clone"></Link> */}
       </Flex>
     </VStack>
   );
