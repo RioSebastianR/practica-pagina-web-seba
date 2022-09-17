31c26756ad54f62d5699e98d9eaee0f6b9928e3d
Sebastian Rios <riossebastianr@gmail.com>
2022-09-16 21:11:21 -0300

    add sub component menuprimary

---
+++ practicas-react/src/components/spotify-clone/sidebar-menu/MenuPrimary.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { VStack, Flex } from "@chakra-ui/react";
+import { HiHome, HiSearch, HiLibrary } from "react-icons/hi";
+import MenuItem from "./sidebar-menu/MenuItem";
+
+function MenuPrimary()
