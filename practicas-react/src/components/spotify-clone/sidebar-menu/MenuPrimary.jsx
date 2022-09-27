6c11187814716c489b3089d4c970f07f1d38ff90
Sebastian Rios <riossebastianr@gmail.com>
2022-09-27 08:04:05 -0300

    delete unused files

--- practicas-react/src/components/spotify-clone/sidebar-menu/MenuPrimary.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { VStack, Flex } from "@chakra-ui/react";
-import { HiHome, HiSearch, HiLibrary } from "react-icons/hi";
-import MenuItem from "./sidebar-menu/MenuItem";
-
-function MenuPrimary()
